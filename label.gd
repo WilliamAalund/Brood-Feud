51d60cea27cac545d9c1fb433332922a1df18bf1
WilliamAalund <williamaalund@icloud.com>
2023-12-02 09:58:36 -0600

    Updated credits

--- label.gd
+++ label.gd
@@ -3,7 +3,7 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	text = "Game Credits:\n\nLead Developer: William Aalund\nLead Artist: Hannia Balderas\nDeveloper 1: Liam Swain\nDeveloper 2: Kieran Kahler\nPlaystation icons: Antoine Plu\nXbox icons: Alric Monteiro"
+	text = "Game Credits:\n\nLead Developer: William Aalund\nLead Artist: Hannia Balderas\nPathfinding, General Development: Liam Swain\nSound, Menu Screen, General Development: Kieran Kahler\nPlaystation icons: Antoine Plu\nXbox icons: Alric Monteiro\nSounds: Various Sources"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
